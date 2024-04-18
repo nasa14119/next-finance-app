f7003c6d9b7ef1520a4b2a4060d57dbf6b88737c
nasa14119 <nico.amaya0205@gmail.com>
2024-04-17 22:54:22 -0600

    feat(gastos): adding enpoint all crud funcionality

--- model/gastos.ts
+++ model/gastos.ts
@@ -1,6 +1,113 @@
 import { db } from "../db"
+import { Gasto } from "../routes/gastos";
 
-export const getGastos = () =>{
+export const getGastos = () : Gasto[] =>{
   const res = db.query("SELECT * FROM gastos"); 
-  return res.all(); 
+  return res.all() as Gasto[]; 
 }
+export const getGasto = (id: string) => {
+  const query = db.query(`
+    SELECT * FROM gastos WHERE id = $id
+  `)
+  return query.get({$id: id}) as Gasto; 
+}
+
+export const createGasto = (gasto : Gasto) => {
+  const value = db.prepare(`
+  INSERT INTO gastos (id, valor, descripcion, dia, mes, ano)
+  VALUES ($id, $valor, $descripcion, $dia, $mes, $ano)
+`)
+  const newGasto = {
+    $id : crypto.randomUUID(), 
+    $valor: gasto.valor,
+    $descripcion: gasto.descripcion,
+    $dia : gasto.dia, 
+    $mes: gasto.mes, 
+    $ano: gasto.mes
+  }
+  value.run(newGasto); 
+  return getGasto(newGasto.$id); 
+}
+type PayloadDate = {
+  id: string, 
+  dia: number, 
+  mes: number, 
+  ano: number
+}
+export const changeDate = (gasto : PayloadDate) => {
+  const isNotFound = getGasto(gasto.id) === null; 
+  if(isNotFound){
+    throw "ID not found in db"
+  }
+  const query = db.prepare(`
+    UPDATE gastos
+    SET dia = $dia, mes = $mes, ano = $ano
+    WHERE id = $id
+  `)
+  const newGasto = {
+    $id: gasto.id, 
+    $dia: gasto.dia, 
+    $mes: gasto.mes, 
+    $ano: gasto.ano
+  }
+  query.run(newGasto); 
+  return getGasto(gasto.id); 
+}
+
+type PayloadValor = {
+  id: string, 
+  valor: number
+}
+export const changeValor = (gasto : PayloadValor) => {
+  if(!gasto.id || !gasto.valor) throw "Payload not valid"
+  const isNotFound = getGasto(gasto.id) === null; 
+  if(isNotFound){
+    throw "ID not found in db"
+  }
+  const query = db.prepare(`
+    UPDATE gastos
+    SET valor = $valor
+    WHERE id = $id
+  `)
+  const newGasto = {
+    $id: gasto.id, 
+    $valor: gasto.valor
+  }
+  query.run(newGasto); 
+  return getGasto(gasto.id); 
+}
+type PayloadDescripcion = {
+  id: string, 
+  descripcion: string
+}
+export const changeDescripcion = (gasto : PayloadDescripcion) => {
+  if(!gasto.id || !gasto.descripcion) throw "Payload not valid"
+  const isNotFound = getGasto(gasto.id) === null; 
+  if(isNotFound){
+    throw "ID not found in db"
+  }
+  const query = db.prepare(`
+    UPDATE gastos
+    SET descripcion = $descripcion
+    WHERE id = $id
+  `)
+  const newGasto = {
+    $id: gasto.id, 
+    $descripcion: gasto.descripcion
+  }
+  query.run(newGasto); 
+  return getGasto(gasto.id); 
+}
+
+export const deleteGasto = (id: string) => {
+  if(!id) throw "No id was provided"
+  const isNotFound = getGasto(id) === null; 
+  if(isNotFound){
+    throw "ID not found in db"
+  }
+  const query = db.prepare(`
+    DELETE FROM gastos WHERE id = $id
+  `)
+  query.run({$id: id})
+  return getGastos(); 
+} 
--- routes/gastos.ts
+++ routes/gastos.ts
@@ -1,10 +1,94 @@
 import { Router } from "express"
-import { getGastos } from "../model/gastos";
+import { changeDate, changeDescripcion, changeValor, createGasto, deleteGasto, getGastos } from "../model/gastos";
+import { z } from "zod";
 const app = Router(); 
-
+const Gasto = z.object({
+  id: z.string().optional(), 
+  valor: z.number(), 
+  descripcion: z.string(), 
+  dia: z.number(), 
+  mes: z.number(), 
+  ano: z.number()
+})
+export type Gasto = z.infer<typeof Gasto>
 app.get("/", (req, res) => {
   const gastos = getGastos(); 
   res.status(200).send(gastos); 
+})
+
+app.post("/" , (req, res) => {
+  const check = Gasto.safeParse(req.body); 
+  if("error" in check){
+    return res.status(400).send(check.error.errors); 
+  }
+  const newGasto = createGasto(req.body); 
+  return res.status(200).send(newGasto); 
+})
+const PayloadDateShema = z.object({
+  id: z.string(), 
+  dia: z.number(), 
+  mes: z.number(), 
+  ano: z.number()
+})
+app.put("/date", (req, res) =>{
+  const check = PayloadDateShema.safeParse(req.body); 
+  if("error" in check){
+    return res.status(400).send(check.error.errors); 
+  }
+  try {
+    const newGastoDate = changeDate(req.body);
+    return res.status(200).send(newGastoDate); 
+  } catch (error) {
+    res.status(404).send({error})
+  }
+})
+
+const PayloadValorShema = z.object({
+  id: z.string(), 
+  valor: z.number()
+})
+app.put("/valor", (req, res) =>{
+  const check = PayloadValorShema.safeParse(req.body); 
+  if("error" in check){
+    return res.status(400).send(check.error.errors); 
+  }
+  try {
+    const newGastoValue = changeValor(req.body);
+    return res.status(200).send(newGastoValue); 
+  } catch (error) {
+    res.status(404).send({error})
+  }
+})
+const PayloadDescripcionShema = z.object({
+  id: z.string(), 
+  descripcion : z.string()
+})
+app.put("/descripcion", (req, res) =>{
+  const check = PayloadDescripcionShema.safeParse(req.body); 
+  if("error" in check){
+    return res.status(400).send(check.error.errors); 
+  }
+  try {
+    const newGastoValue = changeDescripcion(req.body);
+    return res.status(200).send(newGastoValue); 
+  } catch (error) {
+    res.status(404).send({error})
+  }
+})
+const PayloadDeleteShema = z.object({
+  id: z.string(), 
+})
+app.delete("/", (req, res) =>{
+  const check = PayloadDeleteShema.safeParse(req.body); 
+  if("error" in check){
+    return res.status(400).send(check.error.errors); 
+  }
+  try {
+    const newGastosList = deleteGasto(req.body.id);
+    return res.status(200).send(newGastosList); 
+  } catch (error) {
+    res.status(404).send({error})
+  }
 })
 
 export default app; 
