acc8c41765cf084f94bcdabc8ec484f1271b8d41
nasa14119 <nico.amaya0205@gmail.com>
2024-04-21 12:45:50 -0600

    adding sections for gastos and ingresos tables, changes in GraficoCircular call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { AddGastoBtn } from "@components/AgregarGasto"
 import { AddIngresoBtn } from "@components/AgregarIngreso"
 export default function Home() {
   return <>
-    <main className="flex min-h-screen flex-col items-center justify-between md:grid md:grid-cols-2">
-      <section className="h-screen w-screen grid [grid-template-rows:15%_1fr] p-5 md:w-full md:h-full">
+    <main className="flex min-h-screen flex-col items-center justify-between md:grid md:grid-cols-2 p-2 md:gap-x-2 ">
+      <section className="h-screen w-screen grid [grid-template-rows:15%_1fr] grid-cols-1 p-5 md:w-full md:h-full">
         <header className="flex flex-col items-center justify-center">
-          <h1 className="text-xl text-center">Finanzas App</h1>
+          <h1 className="text-xl text-center">Resumen</h1>
         </header>
         <main className=" bg-primary/20 p-2 rounded-[0.5rem] relative flex flex-col justify-end md:flex-row md:items-end md:justify-between">
           <header className="absolute top-2 right-4 left-4 grid grid-cols-2 text-[12px] lg:text-sm text-center gap-y-2 xl:w-1/2 xl:left-auto">
@@ -16,9 +16,22 @@ export default function Home() {
             <div>Dias Faltantes</div>
             <div>00</div>
           </header>
-          <GraficoCircular total={1800} value={1300}/> 
+          <GraficoCircular/> 
           <AddIngresoBtn/>
           <AddGastoBtn /> 
+        </main>
+      </section>
+      <section className="h-screen w-screen grid [grid-template-rows:1fr] md:[grid-template-rows:15%_1fr] p-5 md:w-full md:h-full grid-cols-1">
+        <header className="md:flex flex-col items-center justify-center hidden">
+          <h1 className="text-xl text-center">Nav</h1>
+        </header>
+        <main className="grid grid-rows-2 grid-cols-1 *:rounded-[0.5rem] *:p-5 gap-y-2">
+          <section className="bg-gradient-to-br from-accent/30 to-secondary/30">
+            <h2>Ingresos</h2>
+          </section>
+          <section className="bg-gradient-to-r from-rose/30 to-pink/30 ">
+            <h2>Gastos</h2>
+          </section>
         </main>
       </section>
     </main>
