d210824a21663efa09231efee4344ef1edb12dd1
nasa14119 <nico.amaya0205@gmail.com>
2024-04-21 12:41:44 -0600

    adding colors to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'text': '#ffffff',
+      'background': '#000000',
+      'primary': '#39d9dc',
+      'secondary': '#8ca208',
+      'accent': '#8bf426',
+      'dager': '#d62828', 
+      'black': "#000000", 
+      'white': '#ffffff', 
+      'rose' : '#be143c', 
+      'pink': '#db2777'
+     },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        'text': '#ffffff',
-        'background': '#000000',
-        'primary': '#39d9dc',
-        'secondary': '#8ca208',
-        'accent': '#8bf426',
-        'dager': '#d62828'
-       },
     },
   },
   plugins: [],
