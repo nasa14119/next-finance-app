3ce7698e05f7ade64d78942d8c1ff6b0e27e15d6
nasa14119 <nico.amaya0205@gmail.com>
2024-05-06 21:59:54 -0600

    feat(RowData): No se que decir

--- src/components/RowData/index.tsx
+++ src/components/RowData/index.tsx
@@ -7,10 +7,13 @@ type Props = {
 }
 export function RowData(info:Props) {
   return (
-    <ul className="grid grid-rows-1 [grid-template-columns:1fr_1fr_50%] text-xs md:text-base md:[grid-template-columns:1fr_25%_30%]">
+    <ul className="grid grid-rows-1 [grid-template-columns:25%_30%_1fr] text-xs md:text-base md:[grid-template-columns:1fr_25%_30%]">
       <li>{info.title}</li>
       <li className="text-center md:text-left">${info.value}</li>
-      <li className="text-right">fecha: {info.dia}/{info.mes}/20{info.ano}</li>
+      <li className="text-right flex justify-between w-full md:max-w-36 ml-auto">
+        <span>fecha:</span>
+        <span>{info.dia}/{info.mes}/20{info.ano}</span>
+        </li>
     </ul>
   )
 }
