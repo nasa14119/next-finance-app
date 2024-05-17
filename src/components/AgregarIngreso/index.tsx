dab27e0e227711abd194c1bcced260690233bfd7
nasa14119 <nico.amaya0205@gmail.com>
2024-05-16 20:25:22 -0600

    fix(useAgregarGasto): This is not in use

--- src/components/AgregarIngreso/index.tsx
+++ src/components/AgregarIngreso/index.tsx
@@ -1,9 +1,7 @@
 "use client"
 import { useFormModal } from "@components/DataForm/useFormModal";
 import {AddIcon} from "../../assets/icons"
-import { useAgregarGasto } from "./useAgregarGasto"
 export const AddIngresoBtn = () => {
-  // const handleEvent = useAgregarGasto();
   const [Form, handleState] = useFormModal(); 
   return (
     <div onClick={handleState.open} className="cursor-pointer flex justify-start gap-x-5 h-10 items-center bg-accent/20 mb-5 p-5 rounded-xl md:w-[250px]">
--- src/components/AgregarIngreso/useAgregarGasto.tsx
+++
@@ -1,6 +0,0 @@
-import { useAhorroMethods } from "src/context/app"
-
-export const useAgregarGasto = (): ()=> void => {
-  const methods = useAhorroMethods(); 
-  return () => undefined
-}
