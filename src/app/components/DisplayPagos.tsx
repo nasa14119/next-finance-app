65e0489851c91ecff45dccdcd6ff56e6902edefb
nasa14119 <nico.amaya0205@gmail.com>
2024-06-03 21:16:18 -0600

    feat(DisplyPagos): making changes in funcionality of this component, looking to display information of day for next payment, day for the next payment and time that have been delay

--- src/app/components/DisplayPagos.tsx
+++ src/app/components/DisplayPagos.tsx
@@ -1,54 +1,105 @@
 "use client"
+import moment from "moment";
 import { useIngresosFijosData } from "src/context/app";
 import { getDay, getMonth } from "src/utils";
-
+moment.locale("es")
 export const UltimoPago = () => {
   const data = useIngresosFijosData();
   if (data === null) {
     return <div>Ultimo Pago 00/00/0000</div>;
   }
-  const dia = data.dia_entrada < 10 ? "0" + data.dia_entrada : data.dia_entrada;
-  const mes = data.mes_entrada < 10 ? "0" + data.mes_entrada : data.mes_entrada;
-  const ano = "20" + data.ano_entrada;
+  const time = moment({day: data.dia_entrada, month: data.mes_entrada-1})
   return (
     <div>
-      Ultimo Pago {dia}/{mes}/{ano}
+      Ultimo Pago {time.format("DD/MM/YYYY")}
     </div>
   );
 };
 export const ProximoPago = () => {
   const data = useIngresosFijosData();
   if (data === null) {
-    return <div>Siguiente Pago 00/00/0000</div>;
+    return <div>Dias faltantes 00</div>;
   }
-  const dia = data.dia_entrada < 10 ? "0" + data.dia_entrada : data.dia_entrada;
-  const mes = data.mes_entrada + 1 < 10 ? "0" + (data.mes_entrada + 1): data.mes_entrada
-  const ano = "20" + data.ano_entrada;
+  const timeNextPago = moment({day: data.dia_entrada, month: data.mes_entrada-1}).add(1, "M")
+  const time = timeNextPago.diff(moment(), "days")
   return (
-    <div>
-      Siguiente Pago {dia}/{mes}/{ano}
+    <div className="flex justify-end gap-x-2">
+      <span>
+        Dias faltantes proximo pago
+      </span>
+      <span className="block text-white bg-secondary/20 px-2 rounded-3xl text-sm tabular-nums w-7 text-center">
+        {time}
+      </span>
     </div>
   );
 };
+function BtnPagoRecibido() {
+  return (
+    <div className="flex justify-end gap-x-2">
+      <button className="rounded-3xl bg-primary px-2">Dinero Recibido</button>
+    </div>
+  );
+}
+function BtnUndoPagoRecibido() {
+  return (
+    <button className="rounded-3xl bg-dager px-2 relative group">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-3"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18 18 6M6 6l12 12"
+        />
+      </svg>
+      <span className="absolute text-xs -top-5 w-max -left-12 invisible group-hover:visible bg-dager rounded-3xl px-2">no recibi el dinero</span>
+    </button>
+  );
+}
+function BtnPagoRecibidoWithDelay() {
+  return (
+    <div className="absolute w-40">
+      <button className="rounded-3xl bg-primary px-2 w-full invisible group-hover:visible">
+        Dinero Recibido
+      </button>
+    </div>
+  );
+}
 export const DiasFaltantes = () => {
   const data = useIngresosFijosData();
   if (data === null) {
     return <div>00</div>;
   }
-  const {mes_entrada, dia_entrada} = data
-  const mes_actual = getMonth(); 
-  const dia_actual = getDay(); 
-  const isPass = (mes_actual >= mes_entrada && dia_actual > dia_entrada)
-  if(isPass){
-    return <div >
-      <span className="text-white bg-dager/20 px-2 rounded-3xl text-sm">
-        {dia_actual - dia_entrada}
-      </span>
-    </div>
+  const time = moment({day: data.dia_entrada, month: data.mes_entrada -1})
+  const isPass = moment().isAfter(time)
+  const daysRetraso = time.diff(moment(), "days")
+  if (isPass && data.isDelay) {
+    return (
+      <div className="flex justify-end gap-x-2 group relative cursor-pointer z-50">
+        <span>Dias de retraso</span>
+        <span className="block text-white bg-dager/20 px-2 rounded-3xl text-sm tabular-nums w-7 text-center">
+          {daysRetraso * -1}
+        </span>
+        <BtnPagoRecibidoWithDelay/>
+      </div>
+    );
   }
-  return <div >
-  <span className="text-white bg-accent/20 px-2 rounded-3xl text-sm">
-    {dia_entrada - dia_actual}
-  </span>
-</div>
+  console.log(data);
+  if(data.time_delay > 0 && !data.isDelay){
+    return (
+      <div className="flex justify-end gap-x-2 relative z-50">
+        <span>Recibido con Retraso</span>
+        <BtnUndoPagoRecibido />
+        <span className="block text-white bg-yellow/40 px-2 rounded-3xl text-sm tabular-nums w-7 text-center">
+          {data.time_delay}
+        </span>
+      </div>
+    );
+  }
+  return <BtnPagoRecibido/>
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,9 @@ export default async function Home() {
             <h1 className="text-xl text-center">Resumen</h1>
           </header>
           <main className=" bg-primary/20 p-2 rounded-[0.5rem] relative flex flex-col justify-end md:flex-row md:items-end md:justify-between">
-            <header className="absolute top-2 right-4 left-4 grid grid-cols-2 text-[12px] lg:text-sm text-center gap-y-2 2xl:w-3/5 2xl:left-auto">
+            <header className="absolute top-2 right-4 left-4 grid grid-col-1 text-[12px] lg:text-sm text-right gap-y-2 2xl:w-3/5 2xl:left-auto">
               <UltimoPago/>
               <ProximoPago/>
-              <div className="grid place-content-center">Dias Faltantes</div>
               <DiasFaltantes/>
             </header>
             <GraficoCircular />
