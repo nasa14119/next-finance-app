aa5ab2c5aa541395ff8ed77b20582e70902e0e48
nasa14119 <nico.amaya0205@gmail.com>
2024-06-14 14:52:06 -0600

    fix(ResumenCol): error setting key better aproach with the index key

--- src/components/data/ResumenInfo/components/ResumenCol.tsx
+++ src/components/data/ResumenInfo/components/ResumenCol.tsx
@@ -4,8 +4,8 @@ import { AhorroData } from "@context/types";
 export function ResumenCol({ data }: { data: AhorroData[] }) {
   return (
     <div className="flex flex-col">
-      {data.map((v) => (
-        <div className="flex justify-between text-xs md:text-sm max-h-full overflow-y-scroll" key={v.id}>
+      {data.map((v, i) => (
+        <div className="flex justify-between text-xs md:text-sm max-h-full overflow-y-scroll" key={i}>
           <span>{v.descripcion}</span>
           <span>{v.valor}</span>
         </div>
