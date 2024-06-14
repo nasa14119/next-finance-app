2684d3a110afaa1b113ff349ffd750a745918164
nasa14119 <nico.amaya0205@gmail.com>
2024-06-14 14:29:21 -0600

    feat(DataConfigContext): context to make configuration of how to display de information

--- src/context/data/index.tsx
+++ src/context/data/index.tsx
@@ -2,15 +2,41 @@
 import { ReactNode, createContext, useContext, useState } from "react";
 import { AhorroData } from "../types";
 
+const initialMonths = false
 const Context = createContext({data: []}); 
-
+type ValuesContexConfig = {
+  allMonths: boolean;
+  toogleMonth: (value?: boolean) => void;
+};
+const ContextConfig = createContext({allMonths: initialMonths, toogleMonth: () => {}}); 
 export const DataContext = ({data, children}: {data:any, children : ReactNode}) => {
   const [state, setState] = useState(data);
   return <Context.Provider value={{data : state} as {data:any}}>
     {children}
   </Context.Provider>
 }
-
+export const DataConfig = ({children}: {children : ReactNode}) => {
+  const [allMonths, setAllMonths] = useState(initialMonths);
+  const toogleMonth = (value?:boolean) => {
+    if(typeof value === "undefined") return setAllMonths(prev => !prev); 
+    setAllMonths(value); 
+  }
+  const VALUES : ValuesContexConfig = {
+    allMonths, 
+    toogleMonth
+  }
+  return <ContextConfig.Provider value={VALUES}>
+    {children}
+  </ContextConfig.Provider>
+}
+export const useAllMonths = () => {
+  const { allMonths } = useContext(ContextConfig)
+  return allMonths
+}
+export const useToggleMonths =  () => {
+  const { toogleMonth } = useContext(ContextConfig)
+  return toogleMonth
+}
 export const useDataDB = () => { 
   const { data } = useContext(Context)
   return data as AhorroData[]
