20dd89454d7b695a283219846d3e61855e05f07f
nasa14119 <nico.amaya0205@gmail.com>
2024-06-14 18:40:30 -0600

    feat(DisplaySaldo): showing balance mensual to user

--- src/components/data/sections/DisplaySaldo.tsx
+++ src/components/data/sections/DisplaySaldo.tsx
@@ -4,19 +4,45 @@ import { Fragment} from "react";
 import { AgrupedDataValue, useAgrupatedMonthData } from "./hooks/useAgrupatedMonthData";
 import SaldoRow from "./SaldoRow";
 import { useDataDB } from "@context/data/index";
+const getSumMonth = (data: AgrupedDataValue[]) => {
+  const inicialValue = {total: 0, ingresos: 0, gastos: 0}
+  return data.reduce((prev: typeof inicialValue, v) => {
+    if(v.ingreso){
+      prev.total += v.valor 
+      prev.ingresos +=v.valor
+    }else{
+      prev.total -= v.valor 
+      prev.gastos +=v.valor
+    }
+    return prev
+  },inicialValue)
+}
 export function DisplaySaldo() {
   const data : AgrupedDataValue[] = useDataDB(); 
-  const formatData = useAgrupatedMonthData(data); 
+  const formatData = useAgrupatedMonthData(data);
   return (
-    <div>{
-      formatData.map(([month, values]) => (
-      <Fragment key={month}>
-        <h2 className="capitalize text-xl max-w-[500px] md:mx-auto">{month}</h2>
-        <div className="flex gap-y-2 justify-start flex-col max-w-[500px] md:mx-auto text-sm">
-          {values?.map(value => <SaldoRow value={value} ingreso={value.ingreso} key={value.id}/>)}
-        </div>
-      </Fragment>
-    ))}
+    <div>
+      {
+        formatData.map(([month, values]) => {
+          const totals = getSumMonth(values)
+          console.log(totals);
+          return (
+            <Fragment key={month}>
+              <span className="flex flex-col max-w-[500px] my-3 md:mx-auto">
+                <h2 className="capitalize text-xl md:text-left">{month}</h2>
+                <div className="mx-2 grid items-end [grid-template-columns:3rem_3rem_3rem]">
+                  <strong className="text-sm">${totals.total}</strong>
+                  <strong className="text-sm text-accent">${totals.ingresos}</strong>
+                  <strong className="text-sm text-dager">${totals.gastos}</strong>
+                </div>
+              </span>
+              <div className="flex gap-y-2 justify-start flex-col max-w-[500px] md:mx-auto text-sm">
+                {values?.map(value => <SaldoRow value={value} ingreso={value.ingreso} key={value.id}/>)}
+              </div>
+            </Fragment>
+          ) 
+        })
+      }
     </div>
   )
 }
