d54f48597b7d8cadd43653c3f43d83c07ec082b9
nasa14119 <nico.amaya0205@gmail.com>
2024-06-14 18:50:10 -0600

    feat(DisplaySaldo): showing only current month if the config is set to

--- src/components/data/sections/DisplaySaldo.tsx
+++ src/components/data/sections/DisplaySaldo.tsx
@@ -3,7 +3,9 @@
 import { Fragment} from "react";
 import { AgrupedDataValue, useAgrupatedMonthData } from "./hooks/useAgrupatedMonthData";
 import SaldoRow from "./SaldoRow";
-import { useDataDB } from "@context/data/index";
+import { useAllMonths, useDataDB } from "@context/data/index";
+import { LoadingSkeletonData } from "../LoadingSkeletonData";
+import moment from "moment";
 const getSumMonth = (data: AgrupedDataValue[]) => {
   const inicialValue = {total: 0, ingresos: 0, gastos: 0}
   return data.reduce((prev: typeof inicialValue, v) => {
@@ -17,9 +19,36 @@ const getSumMonth = (data: AgrupedDataValue[]) => {
     return prev
   },inicialValue)
 }
+const month = moment().format("MMMM"); 
 export function DisplaySaldo() {
+  const isActive = useAllMonths()
   const data : AgrupedDataValue[] = useDataDB(); 
   const formatData = useAgrupatedMonthData(data);
+  if(isActive === null ) return <LoadingSkeletonData/>
+  if(isActive) return (
+    <div>
+      {
+        formatData.filter(([v]) => v === month).map(([month, values]) => {
+          const totals = getSumMonth(values)
+          return (
+            <Fragment key={month}>
+              <span className="flex flex-col max-w-[500px] my-3 md:mx-auto">
+                <h2 className="capitalize text-xl md:text-left">{month}</h2>
+                <div className="mx-2 grid items-end [grid-template-columns:3rem_3rem_3rem]">
+                  <strong className="text-sm">${totals.total}</strong>
+                  <strong className="text-sm text-accent">${totals.ingresos}</strong>
+                  <strong className="text-sm text-dager">${totals.gastos}</strong>
+                </div>
+              </span>
+              <div className="flex gap-y-2 justify-start flex-col max-w-[500px] md:mx-auto text-sm">
+                {values?.map(value => <SaldoRow value={value} ingreso={value.ingreso} key={value.id}/>)}
+              </div>
+            </Fragment>
+          ) 
+        })
+      }
+    </div>    
+  )
   return (
     <div>
       {
