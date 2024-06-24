7b7b0975ec56ad004eab3e6fad43ae8116ae4c8b
nicolas amaya <nicolasamaya@MacBook-Pro-de-nicolas.local>
2024-06-23 22:53:44 -0600

    Feat(NetoRow) : adding router to neto row and delete botton

--- src/components/data/sections/NetoRow.tsx
+++ src/components/data/sections/NetoRow.tsx
@@ -1,13 +1,16 @@
+"use client"
 import { Data } from "@context/types";
+import { useRouter } from "next/navigation";
 
 export function NetoRow({
   value,
 }: {
   value: Data;
 }) {
+  const { push } = useRouter()
   return (
-    <a
-      href={`/${value.type}?id=${value.id}`}
+    <div
+      onClick={() => push(`/${value.type}?id=${value.id}`)}
       className={`hover:bg-white/30 grid grid-cols-2 md:[grid-template-columns:1fr_5rem_20%] gap-x-3 mx-2 md:ml-4 px-2 py-2 rounded-md cursor-pointer min-h-8 relative group`}
     >
       <span className="col-span-2 md:col-span-1">{value.descripcion}</span>
@@ -36,6 +39,6 @@ export function NetoRow({
           />
         </svg>
       </span>
-    </a>
+    </div>
   );
 }
