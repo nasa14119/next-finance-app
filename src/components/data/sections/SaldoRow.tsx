2a3e5db049cf838cb660ef9dcbc41af61aa649a6
nasa14119 <nico.amaya0205@gmail.com>
2024-07-01 17:02:05 -0600

    feat(Saldo): Saldo click to send you to data type more informaion

--- src/components/data/sections/SaldoRow.tsx
+++ src/components/data/sections/SaldoRow.tsx
@@ -1,12 +1,17 @@
 import { Data } from "@context/types";
+import { useRouter } from "next/navigation";
 
 export function SaldoRow({
   value,
 }: {
   value: Data;
 }) {
+  const { push } = useRouter()
   return (
-    <div className={`grid grid-cols-2 md:[grid-template-columns:1fr_5rem_20%] gap-x-3 mx-2 md:ml-4 px-2 py-1 rounded-2xl ${value.type === "ingreso" ? "bg-accent/30":"bg-dager/30"}`}>
+    <div
+     className={`cursor-pointer grid grid-cols-2 md:[grid-template-columns:1fr_5rem_20%] gap-x-3 mx-2 md:ml-4 px-2 py-1 rounded-2xl ${value.type === "ingreso" ? "bg-accent/30":"bg-dager/30"}`}
+     onClick={()=> push(`/data/${value.type}s`)}
+     >
       <span className="col-span-2 md:col-span-1">{value.descripcion}</span>
       <span>
         {value.dia}/{value.mes}/{value.ano}
