ba4f92d99ae1b9cdedfb7d3f8160ab65852ec64e
nasa14119 <nico.amaya0205@gmail.com>
2024-12-23 15:47:43 -0600

    Adding some type for some reason?

--- routes/types.ts
+++ routes/types.ts
@@ -11,3 +11,12 @@ export const DataPutSchema = z.object({
 
 export type DataWithId = z.infer<typeof DataPutSchema>
 export type DataPut = z.infer<typeof DataPutSchema>
+
+export const IngresosFijos = z.object({
+  updated: z.string(), 
+  last_payment: z.string().min(1), 
+  salary: z.number().min(1),
+  state: z.number().min(1), 
+  isPay: z.boolean(),
+  style_payment: z.union([z.literal("MONTHLY"), z.literal("BIWEEKLY"), z.literal("WEEKLY")])
+}); 
